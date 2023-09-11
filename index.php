a31b381685f54686ff692e92c9d81e06ba60c2b3
sabihanaikwade26 <sabihanaikwade2622@gmail.com>
2023-09-11 13:42:16 +0530

    Skeleton for user registration form

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Registration Form</title>
+</head>
+<body>
+    <div>
+    <h2>User Registration</h2>
+    <form>
+        
+        <input type="text" name="name" placeholder='Name' required><br>
+
+        <input type="text" name="street_address" placeholder='Street Address' required><br>
+
+        <input type="text" name="city" placeholder='City' required><br>
+
+        <input type="text" name="state" placeholder='State' required><br>
+
+        <input type="text" name="postal_code" placeholder='Postal Code' required><br>
+
+        <button>Submit</button>
+    </form>
+    </div>
+</body>
+</html>
